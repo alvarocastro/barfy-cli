780736a9c1bd5ba47efc60d5a4012822d2cd9d26
Alvaro Castro <moltar89@gmail.com>
2016-08-03 12:57:47 -0300

    .

--- main.js
+++ main.js
@@ -1,89 +1,53 @@
-var WebSocket = require('ws'),
-	inquirer = require('inquirer'),
-	argv = require('yargs').argv;
+#!/usr/bin/env node
 
-if (argv.version || argv.v) {
-	console.log(`
-	                             .-""""-.
-	                            /' .  '. \\
-	                           (\`-..:...-')
-	                            ;-......-;
-	                             '------'
-	      _______     ________    _______     ________    __    __     
-	     / ____ /\\   / ____  /\\  / ____ /\\   / ______/\\  /_/\\ _/_/\\   
-	    / /\\__/_/\\  / /\\__/ / / / /\\__/ /\\  / /\\_____\\/  /_/\\/_/\\\\/  
-	   / ____ /\\\\/ / ____  / / / ____ /\\\\/ / _____/\\     / /_/\\\\/   
-	  / /\\__/_/\\  / /\\__/ / / / /\\  / /\\  / /\\____\\/     / /\\\\/    
-	 /______/\\\\/ /_/ / /_/ / /_/ / /_/ / /_/ /          /_/ /     
-	 \\______\\/   \\_\\/  \\_\\/  \\_\\/  \\_\\/  \\_\\/           \\_\\/     
-	`);
-	process.exit();
-}
+const WebSocket = require('ws'),
+	Vorpal = require('vorpal'),
+	repl = require('vorpal-repl');
+
+const vorpal = Vorpal();
+
 
 var ws = new WebSocket('ws://barfy-server.herokuapp.com');
-/*
-ws.on('open', function () {
+/*ws.on('open', function () {
 	ws.send(JSON.stringify(message));
-});
-*/
+});*/
 ws.on('message', function (message) {
-	//console.log('received: %s', message);
+	//vorpal.log('received: %s', message);
 });
 ws.on('error', function (message) {
-	console.log('ERROR: %s', message);
+	vorpal.log('ERROR: %s', message);
 });
 
+const send = function (action, value) {
+	var message = {type: action, id: value};
 
-const ACTIONS = {
-	'audio': [
-		'aguanta',
-		'botellero'
-	]
-};
-
-var send = (action, value) => {
-	console.log('send', action, value);
-	ws.send(JSON.stringify({
-		type: action,
-		id: value
-	}));
+	//vorpal.log('SEND', message);
+	ws.send(JSON.stringify(message));
 };
 
 
-var promptAction = () => {
-		return inquirer.prompt([{
-			type: 'input',
-			name: 'command',
-			message: '>'
-		}])
-			.then((input) => {
-				var command = input.command.split(' '),
-					action = command[0],
-					value = command[1];
+vorpal
+	.command('audio <value...>', 'Reproduce crap.')
+	.autocomplete(['aguanta', 'botellero', 'dale-ingrid'])
+	.alias('a')
+	.action(function (args, callback) {
+		args.value.forEach((value) => {
+			send('audio', value);
+		});
 
-				if (!value) {
-					return promptOptionsForAction(action);
-				}
+		callback();
+	});
 
-				send(action, value);
+vorpal
+	.command('stop')
+	.alias('s')
+	.action(function (args, callback) {
+		send('stop');
 
-				return promptAction();
-			});
-	},
-	promptOptionsForAction = (action) => {
-		return inquirer.prompt([{
-			type: 'rawlist',
-			name: 'command',
-			message: 'Options:',
-			choices: ACTIONS[action]
-		}])
-			.then((input) => {
-				var value = input.command;
+		callback();
+	});
 
-				send(action, value);
-
-				return promptAction();
-			});
-	};
-
-promptAction();
+vorpal
+	.delimiter('>')
+	.use(repl)
+	.show();
